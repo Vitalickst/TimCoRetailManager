594fac6013d9467d25e53ffbd5bd198ef773653d
Vitalickst <vitalick.voron2014@yandex.ru>
2021-03-25 21:51:35 +0300

    Wire Up Login Form to Api

---
+++ TRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using TRMDesktopUI.Helpers;
+
+namespace TRMDesktopUI.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+        private string _userName;
+        private string _password;
+        private IAPIHelper _apiHelper;
+
+        public LoginViewModel(IAPIHelper apiHelper)
+        {
+            _apiHelper = apiHelper;
+        }
+
+        public string UserName {
+            get => _userName;
+            set 
+            {
+                _userName = value;
+                NotifyOfPropertyChange(() => UserName);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        public string Password {
+            get => _password;
+            set 
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        public bool CanLogIn
+        {
+            get
+            {
+                bool output = UserName?.Length > 0 && Password?.Length > 0;
+
+                return output;
+            }
+        }
+
+        public async Task LogIn()
+        {
+            try
+            {
+                var result = await _apiHelper.Authenticate(UserName, Password);
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -8,8 +8,14 @@ using Caliburn.Micro;
 
 namespace TRMDesktopUI.ViewModels
 {
-    public class ShellViewModel 
+    public class ShellViewModel : Conductor<object>
     {
+        private LoginViewModel _loginVM;
 
+        public ShellViewModel(LoginViewModel loginVM)
+        {
+            _loginVM = loginVM;
+            ActivateItem(_loginVM);
+        }
     }
 }
