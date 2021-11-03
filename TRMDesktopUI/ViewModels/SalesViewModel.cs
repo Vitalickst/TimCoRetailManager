57fc1cbc5fc48362de69710361dcb8b2f3a38704
Vitalickst <vitalick.voron2014@yandex.ru>
2021-11-03 13:35:59 +0300

    Add Additional Item Info

--- TRMDesktopUI/ViewModels/SalesViewModel.cs
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -104,15 +104,6 @@ namespace TRMDesktopUI.ViewModels
                 .Where(x => x.Product.IsTaxable)
                 .Sum(x => x.Product.RetailPrice * x.QuantityInCart * taxRate);
 
-
-            //foreach (var item in Cart)
-            //{
-            //    if (item.Product.IsTaxable)
-            //    {
-            //        taxAmount += (item.Product.RetailPrice * item.QuantityInCart * taxRate);
-            //    }
-            //}
-
             return taxAmount;
         }
 
